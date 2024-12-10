c44600cadef4e71bf677d7d2e10423be0171d96f
AInyatkin <link_alexsey@mail.ru>
2024-12-10 18:22:14 +0700

    auth

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from pycparser.ply.yacc import token
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 import time
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.contrib.auth.models import update_last_login
 
 from users.models import CustomUser
 from users.serializers import LoginSerializer, CustomUserSerializer, RegistrationSerializer
@@ -71,3 +73,24 @@ class RegistrationView(generics.GenericAPIView):
         return Response({"message": "Успешная регистрация"},
                         status=status.HTTP_200_OK)
 
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class AuthView(generics.GenericAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = CustomUserSerializer
+
+    def get(self, request):
+        try:
+            user = request.user
+            serializer = CustomUserSerializer(user)
+            update_last_login(None, user)
+            return Response(serializer.data,
+                            status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"message": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
