48791135830d28a0f40541ce0caab2d42cddea6c
Koval_Dan <koval_dan2004@mail.ru>
2024-12-10 18:13:45 +0700

    Модели в booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
+from users.models import CustomUser
 
-# Create your models here.
+
+class Classroom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    capacity = models.PositiveIntegerField()
+    
+
+    def __str__(self):
+        return self.name
+    
+
+# Модель Booking (Бронирование)
+class Booking(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # Уникальный ID брони
+    classroom = models.ForeignKey(Classroom, related_name='bookings', on_delete=models.CASCADE)  # Связь с Classroom
+    user = models.ForeignKey(CustomUser, related_name='bookings', on_delete=models.CASCADE)  # Связь с User (пользователь)
+    from_time = models.PositiveIntegerField()  # Время начала брони
+    to_time = models.PositiveIntegerField()  # Время окончания брони
+    created_at = models.DateTimeField(auto_now_add=True)  # Дата и время создания брони
+    updated_at = models.DateTimeField(auto_now=True)  # Дата и время последнего обновления
+
+    # def __str__(self):
+    #     return f"Booking for {self.classroom.name} by {self.user.username} from {self.from_time} to {self.to_time}"
+
+    # class Meta:
+    #     # Уникальное ограничение: нельзя создать бронирование, если время пересекается с другим бронированием
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['classroom', 'from_time', 'to_time'], name='unique_booking_time')
+    #     ]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Classroom
+from .serializers import ClassroomSerializer
 
-# Create your views here.
+
+# Доступ для всех пользователей (только просмотр)
+class ClassroomListView(generics.ListCreateAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+    
+# Доступ к конкретому помещению
+class ClassroomDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE']:
+            return [permissions.IsAdminUser]
+        return [permissions.AllowAny()]
+    
+    
